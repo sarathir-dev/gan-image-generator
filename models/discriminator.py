d15ae50d1c370aeb3764c67cf9a64f03ce22a80a
sarathir-dev <sarathir.dev@gmail.com>
2025-07-26 11:41:41 +0530

    Added GAN model components Genrator and Disriminator architectures

---
+++ models/discriminator.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img):
+        return self.model(img)
---
+++ models/generator.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim=100):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(latent_dim, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 28 * 28),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        return self.model(z)
